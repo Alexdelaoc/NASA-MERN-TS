6b59a5dc64744777df992c40094b0fc29d185323
Alexdelaoc <alexdelaoc@gmail.com>
2022-08-10 13:30:55 +0200

    [ADD] landings by name

--- server/src/controllers/controllers.ts
+++ server/src/controllers/controllers.ts
@@ -14,3 +14,17 @@ export const getAllLandings = async (req: Request, res: Response) => {
     }
 };
 
+export const getLandingsByName = async (req: Request, res: Response) => {
+    try {
+        const name  = req.params.name;
+        const query = { name: name };
+        const data = await landingSchema.find(query).exec();
+        if (data.length === 0) {
+            res.status(200).json({msg: "No such landings for the name provided"})
+        } else {
+            res.status(200).json(data)
+        }
+    } catch (error) {
+        res.status(400).json({error: `${error}`})
+    }
+}
--- server/src/routes/apiRoutes.ts
+++ server/src/routes/apiRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express"; // Creating a new Router() object. Importing Express Types.
-import { getAllLandings,} from "../controllers/controllers";
+import { getAllLandings, getLandingsByName} from "../controllers/controllers";
 const apiRouter: Router = Router();
 
 
 // Controllers here 👇
 apiRouter.get('/landings', getAllLandings);
+apiRouter.get('/landings/name/:name', getLandingsByName)
 
 export default apiRouter
