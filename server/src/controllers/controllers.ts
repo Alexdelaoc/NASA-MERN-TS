f81fbbfb4b2a5a5b83d180d88b1d8c1f40a6ef18
Alexdelaoc <alexdelaoc@gmail.com>
2022-08-10 13:38:47 +0200

    [ADD] landings by mass

--- server/src/controllers/controllers.ts
+++ server/src/controllers/controllers.ts
@@ -17,10 +17,25 @@ export const getAllLandings = async (req: Request, res: Response) => {
 export const getLandingsByName = async (req: Request, res: Response) => {
     try {
         const name  = req.params.name;
-        const query = { name: name };
-        const data = await landingSchema.find(query).exec();
-        if (data.length === 0) {
+        const filter = { name: name };
+        const data = await landingSchema.find(filter).exec();
+        if (!data) {
             res.status(200).json({msg: "No such landings for the name provided"})
+        } else {
+            res.status(200).json(data)
+        }
+    } catch (error) {
+        res.status(400).json({error: `${error}`})
+    }
+}
+
+export const getLandingsByMass = async (req: Request, res: Response) => {
+    try {
+        const mass = req.params.mass;
+        const filter = { mass: mass };
+        const data = await landingSchema.find(filter, "-_id");
+        if (!data) {
+            res.status(200).json({msg: "No such landings for the mass provided"})
         } else {
             res.status(200).json(data)
         }
--- server/src/routes/apiRoutes.ts
+++ server/src/routes/apiRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express"; // Creating a new Router() object. Importing Express Types.
-import { getAllLandings, getLandingsByName} from "../controllers/controllers";
+import { getAllLandings, getLandingsByName, getLandingsByMass} from "../controllers/controllers";
 const apiRouter: Router = Router();
 
 
 // Controllers here 👇
 apiRouter.get('/landings', getAllLandings);
-apiRouter.get('/landings/name/:name', getLandingsByName)
+apiRouter.get('/landings/name/:name', getLandingsByName);
+apiRouter.get('/landings/mass/:mass', getLandingsByMass);
+
 
 export default apiRouter
