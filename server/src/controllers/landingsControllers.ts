3c7f0068f0ac581aa87607758dbd46e3eed5da59
Alexdelaoc <alexdelaoc@gmail.com>
2022-08-24 22:11:29 +0200

    [FIX] moved neas controllers to another file

--- server/src/controllers/landingsControllers.ts
+++ server/src/controllers/landingsControllers.ts
@@ -1,12 +1,7 @@
 // REQUERIMENTOS
 import { Request, Response } from "express";
-import { CallbackError } from "mongoose";
-import LandingSchema, { ILanding } from "./../models/landings";
-import NeaSchema, { INea } from "./../models/neas";
-import { getLandingsByParams, getLandingsByQuery, getLandingsFromYear, getLandingsToYear, getLandingsInRange } from "./../utils/landingUtils";
-
-
-// LANDINGS //
+import LandingSchema, { ILanding } from "../models/landings";
+import { getLandingsByParams, getLandingsByQuery, getLandingsFromYear, getLandingsToYear, getLandingsInRange } from "../utils/landingUtils";
 
 // Defining the Interfaces from the Request Object so it gives no errors:
 interface RequestParams { };
@@ -178,62 +173,5 @@ export const deleteLanding = async (req: Request, res: Response) => {
     } catch (error) {
         console.error(error);
         res.status(400).json({ msg: "Something went wrong. Bad request." });
-    }
-};
-
-export const getAllNeas = async (req: Request, res: Response) => {
-    try {
-        let data = await NeaSchema.find({}, "-_id");
-        data.length == 0
-            ? res.status(400).json({ msg: "Something went wrong" })
-            : res.status(200).json(data)
-    } catch (error) {
-        console.error(error);
-        res.status(400).json({ msg: "Something went wrong. Bad request." })
-    }
-};
-
-export const createNeas = async (req: Request, res: Response) => {
-    try {
-        const { designation, discovery_date, h_mag, moid_au, q_au_1, q_au_2, period_yr, i_deg, pha, orbit_class } = req.body;
-        const newNea: INea = await new NeaSchema(req.body);
-        newNea.save((err: CallbackError, newNea: INea) => {
-            err
-                ? res.status(200)
-                : res.status(201).json({ msg: `${newNea.designation}, ${newNea.orbit_class} saved in the database successfully.` })
-        })
-    } catch (error) {
-        res.status(400).json({ msg: "Something went wrong. Bad request." })
-    }
-};
-
-export const editNea = async (req: Request, res: Response) => {
-    try {
-        const { designation, discovery_date, h_mag, moid_au, q_au_1, q_au_2, period_yr, i_deg, pha, orbit_class } = req.body;
-        const update = req.body;
-        const filter = { designation: designation };
-        await NeaSchema.findOneAndUpdate(filter, update, { new: true })
-            .then(result => {
-                res.status(201).json({ msg: `Near Earth Object with designation ${filter.designation} was updated successfully: ${result}` })
-            })
-    } catch (error) {
-        console.error(error);
-        res.status(400).json({ msg: "Something went wrong. Bad request." })
-    }
-};
-
-export const deleteNeas = async (req: Request, res: Response) => {
-    try {
-        const { designation } = req.body;
-        const filter = { designation: designation }
-        NeaSchema.deleteMany(filter)
-            .then(result => {
-                result.deletedCount == 1
-                    ? res.status(200).json({ msg: `${result.deletedCount} landing deleted.` })
-                    : res.status(200).json({ msg: `${result.deletedCount} landings deleted.` })
-            })
-    } catch (error) {
-        console.error(error);
-        res.status(400).json({ msg: "Something went wrong. Bad request." })
     }
 };
