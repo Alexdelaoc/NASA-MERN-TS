64ce589ab658a30e8cef2d5545c33d02d076d21a
Alexdelaoc <alexdelaoc@gmail.com>
2022-08-24 19:11:05 +0200

    [ADD] dates controller (not working yet)

--- server/src/controllers/apiControllers.ts
+++ server/src/controllers/apiControllers.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 import { CallbackError } from "mongoose";
 import LandingSchema, { ILanding } from "./../models/landings";
 import NeaSchema, { INea } from "./../models/neas";
-import { getLandingsByParams, getLandingsByQuery } from "./../utils/landingUtils";
+import { getLandingsByParams, getLandingsByQuery, getLandingsFromYear, getLandingsToYear, getLandingsInRange } from "./../utils/landingUtils";
 
 
 // LANDINGS //
@@ -28,10 +28,51 @@ export const getLandings = async (req: Request<RequestParams, ResponseBody, Requ
             } else {
                 res.status(200).json(data)
             }
+            
+        } else if (req.query.from && req.query.to) {
+            const years = {
+                from: req.query.from,
+                to: req.query.to
+            };
+            const data = await getLandingsInRange(years);
+            if (!data || data.length === 0) {
+                res.status(200).json({ 
+                    msg: "No such landings for the mass provided.",
+                    log: `${console.log(data)}` 
+                })  
+            } else {
+                res.status(200).json(data)
+            }
+
+        } else if (req.query.from) {
+            const years = { from: req.query.from };
+            const data = await getLandingsFromYear(years);
+            if (!data || data.length === 0) {
+                res.status(200).json({ 
+                    msg: "No such landings for the mass provided.",
+                    log: `${console.log(data)}` 
+                })  
+            } else {
+                res.status(200).json(data)
+            }
+
+        } else if (req.query.to) {
+            const years = { from: req.query.to };
+            const data = await getLandingsToYear(years);
+            if (!data || data.length === 0) {
+                res.status(200).json({ 
+                    msg: "No such landings for the mass provided.",
+                    log: `${console.log(data)}` 
+                })  
+            } else {
+                res.status(200).json(data)
+            }
+
         } else {
             let data = await LandingSchema.find({}, "-_id");
             res.status(200).json(data);
         }
+
     } catch (error) {
         console.log(error)
         res.status(400).json({
@@ -78,28 +119,15 @@ export const getLandingsByMass = async (req: Request, res: Response) => {
     }
 };
 
-export const getLandingsByMinMass = async (req: Request, res: Response) => {
-    try {
-        const minimum_mass = req.params.minimum_mass;
-        const landingByMass = await LandingSchema.find({ mass: { $gte: minimum_mass } }, "-_id").sort({ mass: 1 });
-        res.status(200).json(landingByMass)
-    }
-    catch (err) {
-        console.log(err);
-        res.status(400).json({ msg: "Something went wrong. Bad request." })
-    };
-
-};
-
 export const getLandingsByClass = async (req: Request, res: Response) => {
     try {
         const recclass = req.params.class;
         const filter = { recclass: recclass.toUpperCase() };
-        const query = await LandingSchema.find(filter, "-_id");
-        if (query.length == 0) {
+        const data = await LandingSchema.find(filter, "-_id");
+        if (data.length == 0) {
             res.status(200).json({ msg: "No such landings for the class provided." })
         } else {
-            res.status(200).json(query)
+            res.status(200).json(data)
         }
     } catch (err) {
         console.log(err);
--- server/src/utils/landingUtils.ts
+++ server/src/utils/landingUtils.ts
@@ -7,16 +7,44 @@ export const getLandingsByQuery = async (mass: Number) => {
     }
     catch (err) {
         console.log(err);
-    };
-}
+    }
+};
+
+export const getLandingsInRange = async (years: any) => {
+    try {
+        console.log(years);
+        const data = await LandingSchema.find({year: {$gte: years.from, $lt: years.to}});
+        return data
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const getLandingsFromYear = async (years: any) => {
+    try {
+        const data = await LandingSchema.find({year: {$gte: years.from}});
+        return data
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const getLandingsToYear = async (years: any) => {
+    try {
+        const data = await LandingSchema.find({year: {$lte: years.to}});
+        return data
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 export const getLandingsByParams = async (mass: any) => {
     try {
         if (mass) {
-            const data = await LandingSchema.find({ mass: mass }, "-_id").exec();
+            const data = await LandingSchema.find({ mass: mass }, "-_id").sort({ mass: 1 });
             return data;
         }
     } catch (error) {
         console.log(error);
     }
-}
+};
