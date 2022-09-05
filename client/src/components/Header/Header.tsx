095bf325b1fbffce93d94ed2dda545fa6103339c
Alexdelaoc <alexdelaoc@gmail.com>
2022-09-05 16:53:51 +0200

    [ADD] setup for client

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+type HeaderProps = {
+  //
+};
+
+class Header extends Component<HeaderProps, any> {
+  public render() {
+    return <div>This is the header</div>;
+  }
+}
+
+export default Header;
---
+++ client/src/components/Main/Main.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+type MainProps = {
+  //
+};
+
+class Main extends Component<MainProps, any> {
+  public render() {
+    return <div>This is the main</div>;
+  }
+}
+
+export default Main;
