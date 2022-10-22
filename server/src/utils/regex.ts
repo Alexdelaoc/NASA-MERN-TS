5a583bdadee0572ebd205753eb9d569f630eecc9
Alexdelaoc <alexdelaoc@gmail.com>
2022-10-22 17:14:00 +0200

    [ADD] regex for email and password

---
+++ server/src/utils/regex.ts
@@ -0,0 +1,14 @@
+const validateEmail = (email: string) => {
+    const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return regexEmail.test(email.toLowerCase())
+};
+
+const validatePassword = (password: string) => {
+    const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*\_\-])(?=.{8,})/
+    return regexPassword.test(password)
+};
+
+export const regex = {
+    validateEmail,
+    validatePassword
+}
